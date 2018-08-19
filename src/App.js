a71382849df959f58a3b36421f694a64921952a3
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-08-19 14:33:53 +0300

    Class components to Functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const list = [
   {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
     objectID: 0
   },
   {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
     objectID: 1
@@ -55,12 +55,14 @@ class App extends Component {
     const {list, searchTerm} = this.state;
     
     return (
-      <div className="App">
+      <div className='App'>
 
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         
         <Table
           list={list}
@@ -73,49 +75,44 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const {value, onChange} = this.props;
-    
-    return (
-      <form>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children}<input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => onDismiss(item.objectID)}
-                type="button"
-              >
-                Отбросить
-              </button>
-            </span>
-          </div>
-        )}
+const Table = ({list, pattern, onDismiss}) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+          >
+            Delete
+          </Button>
+        </span>
       </div>
-    );
-  }
-}
+    )}
+  </div>
+
+
+const Button = ({onClick, className = '', children}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type='button'
+  >
+    {children}
+  </button>
 
 export default App;
